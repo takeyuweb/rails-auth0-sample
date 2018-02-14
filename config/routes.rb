33519ede608fc9991ad4578e8c59d3b3a503217c
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2018-02-15 00:17:43 +0900

    Check the User's Authentication Status

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'dashboard' => 'dashboard#show'
   get '/auth/oauth2/callback' => 'auth0#callback'
   get '/auth/failure' => 'auth0#failure'
   root 'home#show'
